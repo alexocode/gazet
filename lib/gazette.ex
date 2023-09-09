1f426a4f23e222edecf2774f47a12f052ac017da
Alex Wolf <craft@alexocode.dev>
2023-09-09 23:05:26 +0200

    Initial Commit

---
+++ lib/gazette.ex
@@ -0,0 +1,18 @@
+defmodule Gazette do
+  @moduledoc """
+  Documentation for `Gazette`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gazette.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
