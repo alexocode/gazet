18466ad3036d0c6d6a5dc818d0a8429245f2e6aa
Alexandra <alexocode@mailbox.org>
2023-09-18 17:31:27 +0200

    :truck: Rename to Gazet (the flamish spelling)

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Gazette.MixProject do
+defmodule Gazet.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :gazette,
+      app: :gazet,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
@@ -15,7 +15,7 @@ defmodule Gazette.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Gazette.Application, []}
+      mod: {Gazet.Application, []}
     ]
   end
 
