2dd4892485eda73341d2d3e630e30227d8de6d0c
Alex Wolf <craft@alexocode.dev>
2023-09-22 13:07:30 +0200

    :safety_vest: config: Add a module to validate config (defaulting to NimbleOptions)

---
+++ lib/gazet/config/nimble_options.ex
@@ -0,0 +1,17 @@
+if Code.loaded?(NimbleOptions) do
+  defmodule Gazet.Config.NimbleOptions do
+    @behaviour Gazet.Config
+
+    @impl true
+    defdelegate schema!(schema), to: NimbleOptions, as: :new!
+
+    @impl true
+    defdelegate docs(schema), to: NimbleOptions
+
+    @impl true
+    defdelegate validate(config, schema), to: NimbleOptions
+
+    @impl true
+    defdelegate validate!(config, schema), to: NimbleOptions
+  end
+end
