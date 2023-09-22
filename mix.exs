8652f4aaaa45e305edbcdd5799897b98e6a6141c
Alexandra <alexocode@mailbox.org>
2023-09-22 10:36:55 +0200

    :wrench: mix: Adjust for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,29 +1,120 @@
 defmodule Gazet.MixProject do
   use Mix.Project
 
+  @app :gazet
+  @name "Gazet"
+  @description "Publish and subscribe to the latest news ... uhm events"
+  @repo "https://github.com/alexandra-wolf/#{@app}"
+
   def project do
     [
-      app: :gazet,
-      version: "0.1.0",
-      elixir: "~> 1.15",
+      app: @app,
+      elixir: ">= 1.7.4 and < 2.0.0",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      test_coverage: [tool: ExCoveralls],
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: aliases(),
+      deps: deps(),
+
+      # Deps
+      dialyzer: dialyzer(),
+
+      # Docs
+      name: @name,
+      source_url: @repo,
+      homepage_url: @repo,
+
+      # Hex
+      description: @description,
+      docs: docs(),
+      package: package(),
+      version: version()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Gazet.Application, []}
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "check.all": ["format --check-formatted", "credo", "dialyzer"]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # No Runtime
+      {:credo, ">= 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 1.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+
+      # Test
+      {:excoveralls, "~> 0.16", only: :test},
+      {:mox, "~> 1.0", only: :test}
     ]
+  end
+
+  defp dialyzer do
+    [
+      ignore_warnings: ".dialyzer_ignore.exs",
+      plt_add_apps: [],
+      plt_file: {:no_warn, ".dialyzer/dialyzer.plt"}
+    ]
+  end
+
+  #######
+  # Hex #
+  #######
+
+  @extras Path.wildcard("pages/**/*.md")
+  def docs do
+    [
+      main: @name,
+      source_ref: "v#{version()}",
+      source_url: @repo,
+      extras: @extras,
+      groups_for_modules: []
+    ]
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "CHANGELOG*", "LICENSE*", "README*", "version"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo},
+      maintainers: ["Alexandra Wolf <alexwolf1991@>"]
+    ]
+  end
+
+  @version_file "version"
+  def version do
+    cond do
+      File.exists?(@version_file) ->
+        @version_file
+        |> File.read!()
+        |> String.trim()
+
+      System.get_env("REQUIRE_VERSION_FILE") == "true" ->
+        exit("Version file (`#{@version_file}`) doesn't exist but is required!")
+
+      true ->
+        "0.0.0-dev"
+    end
   end
 end
