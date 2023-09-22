e538da7b055d1ee09713095bb4ba97d7ec139580
Alexandra <alexocode@mailbox.org>
2023-09-22 12:22:36 +0200

    :construction: message: Remove topic from message to avoid confusing duplication

--- lib/gazet/message.ex
+++ lib/gazet/message.ex
@@ -3,10 +3,9 @@ defmodule Gazet.Message do
   @type metadata :: map
 
   @type t :: %__MODULE__{
-          topic: Gazet.topic(),
           data: data,
           metadata: metadata
         }
-  @enforce_keys [:topic, :data, :metadata]
-  defstruct [:topic, :data, :metadata]
+  @enforce_keys [:data, :metadata]
+  defstruct [:data, :metadata]
 end
