049510d6100fd328c8cf3b02ba4296618d44b5ba
Alexandra <alexocode@mailbox.org>
2023-10-04 22:37:41 +0200

    :sparkles: config: Add some more helper functions

--- lib/gazet/config.ex
+++ lib/gazet/config.ex
@@ -6,24 +6,30 @@ defmodule Gazet.Config do
   to a minimal implementation where it checks the presence of expected keys.
   """
 
-  @type schema :: term
-
-  @callback schema!(keyword) :: schema
-  @callback docs(schema) :: String.t()
-  @callback typespec(schema) :: Macro.t()
-  @callback validate(config, schema) :: {:ok, config} | {:error, reason :: term}
-            when config: keyword
-  @callback validate!(config, schema) :: config | no_return when config: keyword
-
-  if Code.loaded?(__MODULE__.NimbleOptions) do
+  if Code.ensure_loaded?(NimbleOptions) do
     @implementation __MODULE__.NimbleOptions
   else
     @implementation __MODULE__.Fallback
   end
 
+  @type schema :: term
+  @type error :: @implementation.error()
+
+  @callback schema!(keyword) :: schema
+  @callback docs(schema) :: String.t()
+  @callback map(schema, mapper :: (field :: atom, spec :: keyword -> mapped)) :: list(mapped)
+            when mapped: term
+  @callback typespec(schema) :: Macro.t()
+  @callback typespec(schema, field :: atom) :: Macro.t()
+  @callback validate(config, schema) :: {:ok, config} | {:error, reason :: term}
+            when config: keyword
+  @callback validate!(config, schema) :: config | no_return when config: keyword
+
   defdelegate schema!(keyword), to: @implementation
+  defdelegate map(schema, mapper), to: @implementation
   defdelegate docs(schema), to: @implementation
   defdelegate typespec(schema), to: @implementation
+  defdelegate typespec(schema, field), to: @implementation
   defdelegate validate(config, schema), to: @implementation
   defdelegate validate!(config, schema), to: @implementation
 end
