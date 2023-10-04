45b5b26c5b524cacc7f7636351c280924b9260bf
Alex Wolf <craft@alexocode.dev>
2023-10-05 01:09:54 +0200

    :construction: adapter/local: Add a Registry-based adapter

---
+++ lib/gazet/adapter/local.ex
@@ -0,0 +1,33 @@
+defmodule Gazet.Adapter.Local do
+  alias Gazet.Message
+  alias Gazet.Subscriber
+
+  @behaviour Gazet.Adapter
+
+  @impl true
+  def child_spec(%{name: name}) do
+    Registry.child_spec(
+      keys: :duplicate,
+      name: name,
+      partitions: System.schedulers_online()
+    )
+  end
+
+  @impl true
+  def publish(%{name: name, topic: topic}, %Message{} = message) do
+    Registry.dispatch(name, topic, fn entries ->
+      for {pid, _} <- entries, do: send(pid, {:message, topic, message})
+    end)
+  end
+
+  @impl true
+  def subscriber_spec(%{name: name, topic: topic}, %Subscriber{} = subscriber) do
+    subscriber
+    |> put_in([Access.key!(:adapter_opts), :on_start], fn ->
+      with {:ok, _} <- Registry.register(name, topic, :ignored) do
+        :ok
+      end
+    end)
+    |> Subscriber.Generic.child_spec()
+  end
+end
