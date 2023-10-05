b933e183431cb609c205c39667c381cc4639d41d
Alex Wolf <craft@alexocode.dev>
2023-10-05 15:44:22 +0200

    :construction: subscriber/generic: Add a schema for options and some helpers

--- lib/gazet/subscriber/generic.ex
+++ lib/gazet/subscriber/generic.ex
@@ -1,20 +1,97 @@
+schema =
+  Gazet.Options.schema!(
+    child_spec: [type: :keyword_list, default: [], doc: "Overrides for `child_spec/1`"],
+    start_opts: [type: :keyword_list, default: [], doc: "Options for `GenServer.start_link/3`"],
+    on_start: [
+      type: {:or, [:mfa, {:fun, 0}, {:fun, 1}]},
+      required: false,
+      doc:
+        "Function to be invoked on start, needs to conform to `t:on_start`. When this returns an error the `GenServer` `stop`s with the error reason."
+    ]
+  )
+
+schema_keys = Gazet.Options.map(schema, fn key, _spec -> key end)
+
 defmodule Gazet.Subscriber.Generic do
+  @moduledoc """
+  ## `Subscriber.adapter_opts[:server]`
+  #{Gazet.Options.docs(schema)}
+  """
   use GenServer
 
   alias Gazet.Subscriber
 
-  def child_spec(%Subscriber{adapter_opts: opts} = subscriber) do
+  @schema schema
+
+  @type opts :: [unquote(Gazet.Options.typespec(schema))]
+  @type on_start ::
+          (Subscriber.spec() -> :ok | {:ok, Subscriber.spec()} | {:error, reason :: any})
+
+  for key <- schema_keys do
+    @spec with_opt(Subscriber.spec(), unquote(key), unquote(Gazet.Options.typespec(schema, key))) ::
+            Subscriber.spec()
+  end
+
+  def with_opt(%Subscriber{} = subscriber, key, value) when key in unquote(schema_keys) do
+    update_in(subscriber.adapter_opts[:server], fn
+      nil -> [{key, value}]
+      server_opts when is_list(server_opts) -> Keyword.put(server_opts, key, value)
+    end)
+  end
+
+  @spec with_opts(Subscriber.spec(), opts) :: Subscriber.spec()
+  def with_opts(%Subscriber{} = subscriber, server_opts) do
+    Enum.reduce(server_opts, subscriber, fn {key, value}, subscriber ->
+      with_opt(subscriber, key, value)
+    end)
+  end
+
+  @spec child_spec(Subscriber.spec(), server_opts :: opts) :: Supervisor.child_spec()
+  def child_spec(%Subscriber{} = subscriber, server_opts \\ []) do
+    {:ok, subscriber} =
+      subscriber
+      |> with_opts(server_opts)
+      |> with_validated_opts()
+
     Supervisor.child_spec(
       %{
         id: subscriber.id,
         start: {__MODULE__, :start_link, [subscriber]}
       },
-      opts[:child_spec] || []
+      opt(subscriber, :child_spec)
     )
   end
 
-  def start_link(%Subscriber{} = subscriber) do
-    GenServer.start_link(__MODULE__, subscriber, subscriber.adapter_opts[:start_opts] || [])
+  @spec start_link(Subscriber.spec(), server_opts :: opts) ::
+          GenServer.on_start() | Gazet.Options.error()
+  def start_link(%Subscriber{} = subscriber, server_opts \\ []) do
+    subscriber = with_opts(subscriber, server_opts)
+
+    with {:ok, subscriber} <- with_validated_opts(subscriber) do
+      GenServer.start_link(
+        __MODULE__,
+        subscriber,
+        opt(subscriber, :start_opts)
+      )
+    end
+  end
+
+  defp with_validated_opts(%Subscriber{adapter_opts: adapter_opts} = subscriber) do
+    server_opts = adapter_opts[:server] || []
+
+    if server_opts[:__validated__] == true do
+      {:ok, subscriber}
+    else
+      with {:ok, server_opts} <- Gazet.Options.validate(server_opts, @schema) do
+        server_opts = Keyword.put(server_opts, :__validated__, true)
+
+        {:ok, put_in(subscriber.adapter_opts[:server], server_opts)}
+      end
+    end
+  end
+
+  def opt(%Subscriber{adapter_opts: adapter_opts}, key) do
+    get_in(adapter_opts, [:server, key])
   end
 
   def init(%Subscriber{module: module} = subscriber) do
@@ -22,7 +99,7 @@ defmodule Gazet.Subscriber.Generic do
       {
         :ok,
         %{subscriber | config: config},
-        {:continue, {:on_start, subscriber.adapter_opts[:on_start]}}
+        {:continue, {:on_start, opt(subscriber, :on_start)}}
       }
     end
   end
