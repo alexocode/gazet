acf38c24913a305908d45f7c6b4ded6033aced56
Alex Wolf <craft@alexocode.dev>
2024-01-12 13:44:22 +0100

    :recycle: env: Make resolve/* handle all kinds of call shapes

--- lib/gazet/env.ex
+++ lib/gazet/env.ex
@@ -1,34 +1,45 @@
 defmodule Gazet.Env do
   @moduledoc false
 
-  @type env_spec :: {otp_app :: atom, module :: module}
+  @type env_spec :: {otp_app, module}
+  @type otp_app :: atom
 
-  @spec resolve(spec :: env_spec | [env_spec]) :: keyword
-  def resolve(env_specs) do
-    env_specs
-    |> List.wrap()
-    |> Enum.reduce([], fn {otp_app, module}, merged ->
+  @spec resolve(env_spec | [env_spec]) :: keyword
+  def resolve({otp_app, module}), do: resolve(otp_app, module)
+
+  def resolve(env_specs) when is_list(env_specs) do
+    Enum.reduce(env_specs, [], fn {otp_app, module}, merged ->
       merge(merged, otp_app, module)
     end)
   end
 
-  @spec resolve(spec :: env_spec | [env_spec], allowed_keys :: [atom]) :: keyword
+  @spec resolve(otp_app, module) :: keyword
+  def resolve(otp_app, module) when is_atom(otp_app) and is_atom(module) do
+    Application.get_env(otp_app, module, [])
+  end
+
+  @spec resolve(env_spec | [env_spec], allowed_keys :: [atom]) :: keyword
   def resolve(env_specs, allowed_keys) do
     env_specs
     |> resolve()
     |> Keyword.take(allowed_keys)
   end
 
-  @spec merge(keyword | map, otp_app :: atom, module :: atom) :: keyword
+  @spec resolve(otp_app, module, allowed_keys :: [atom]) :: keyword
+  def resolve(otp_app, module, allowed_keys) when is_atom(otp_app) and is_atom(module) do
+    otp_app
+    |> resolve(module)
+    |> Keyword.take(allowed_keys)
+  end
+
+  @spec merge(keyword | map, otp_app, module) :: keyword
   def merge(into, otp_app, module) when is_map(into) do
     into
     |> Map.to_list()
     |> merge(otp_app, module)
   end
 
-  def merge(into, otp_app, module) do
-    env = Application.get_env(otp_app, module, [])
-
-    Keyword.merge(into, env)
+  def merge(into, otp_app, module) when is_list(into) do
+    Keyword.merge(into, resolve(otp_app, module))
   end
 end
